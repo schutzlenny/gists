672945d09cdb670bc98a22ad304ff127fcccecb2
Jose Pedro Saraiva <js@simplesurance.de>
2020-03-14 23:44:40 +0100

    Improve installer and remove obsolete one

--- workspace.sh
+++ workspace.sh
@@ -1,55 +1,92 @@
 #!/usr/bin/env bash
-INSTALL_PATH="$1"
-REPO_VERSION="${2-develop}"
-
 set -eo pipefail
+SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 
-if [ -z "$INSTALL_PATH" ]; then
-    echo "ERR: No install path specified, please provide it as the first argument to this script" >&2
-    exit 1
-fi
+parse_args() {
+    INSTALL_PATH="${1-$SCRIPT_DIR}"
+    REPO_VERSION="${2-develop}"
 
-INSTALL_PATH="${INSTALL_PATH/#~/$HOME}"
-CHECKOUT_PATH="$INSTALL_PATH/src/sisu.sh"
+    INSTALL_PATH="${INSTALL_PATH/#~/$HOME}"
+    CHECKOUT_PATH="$INSTALL_PATH/src/sisu.sh"
 
-cat <<'EOF'
- _       __           __
-| |     / /___  _____/ /___________  ____ _________
-| | /| / / __ \/ ___/ //_/ ___/ __ \/ __ `/ ___/ _ \
-| |/ |/ / /_/ / /  / ,< (__  ) /_/ / /_/ / /__/  __/
-|__/|__/\____/_/  /_/|_/____/ .___/\__,_/\___/\___/
-                           /_/ simplesurance GmbH
+    if [ -z "$INSTALL_PATH" ]; then
+        echo "ERR: No install path specified, please provide it as the first argument to this script" >&2
+        exit 1
+    fi
+}
 
-EOF
-echo " $(uname -s)/$(uname -r) @ $(uname -m)"
-echo
-echo " * Install path     $INSTALL_PATH"
-echo " * Checkout path    $CHECKOUT_PATH"
-echo " * Version          $REPO_VERSION"
-echo
-echo
-echo " The installer script will now clone simplesurance's monolithic"
-echo " repository to $CHECKOUT_PATH and proceed with installing the"
-echo " minimum requirements to run the development stack."
-echo
-echo " Do you wish to continue?"
-echo " Press any key to continue or [Ctrl^C] to abort"
-echo
-read -n1 -s
-echo
-echo
+function init() {
+    if [ ! -d "$INSTALL_PATH" ]; then
+        echo "WARN: Install path does not exist, creating..."
+        mkdir -p "$INSTALL_PATH"
+    fi
 
-mkdir -p "$CHECKOUT_PATH"
-cd "$CHECKOUT_PATH"
+    if [ ! -d "$CHECKOUT_PATH" ]; then
+        mkdir -p "$CHECKOUT_PATH"
+    fi
 
-# only clone if we haven't cloned already
-if [ -d .git ]; then
-    echo "WARN: Repository clone seems to exist already, skipping..." >&2
-else
-    git clone git@github.com:simplesurance/sisu.git --branch "$REPO_VERSION" .
-fi
+    # resolve paths
+    INSTALL_PATH="$(cd "$INSTALL_PATH"; pwd -P)"
+    CHECKOUT_PATH="$(cd "$CHECKOUT_PATH"; pwd -P)"
+}
 
-cd workspace
-make clean # remove any leftovers from previous installations, if any
-make
-make init
+function banner() {
+    echo
+    echo ".======================== WORKSPACE INSTALLER =========================="
+    echo "|"
+    echo "|"
+    echo "|  Install path     $INSTALL_PATH"
+    echo "|  Checkout path    $CHECKOUT_PATH"
+    echo "|  Version          $REPO_VERSION"
+    echo "|"
+    echo "|"
+    echo "|-----------------------------------------------------------------------"
+    echo "|"
+    echo "| The installer script will now proceed and:"
+    echo "|"
+    echo "| * clone the monorepo, ie: https://github.com/simplesurance/sisu"
+    echo "| * ensure your environment is properly configured"
+    echo "| * ensure your system meets the minimum requirements"
+    echo "| * install the required dependencies to run the dev stack"
+    echo "|"
+    echo "|"
+    echo "| If the installation fails just re run this script to resume it!"
+    echo "|"
+    echo "|"
+    echo "| Coffee? Mate?"
+    echo "| Make yourself confortable, this might take a while!"
+    echo "|"
+    echo "'-----------------------------------------------------------------------"
+    echo
+    prompt_continue
+    echo
+    echo
+}
+
+function prompt_continue() {
+    echo " Do you wish to continue?"
+    echo " Press any key to continue or [Ctrl^C] to abort"
+    echo
+    read -n1 -s
+}
+
+function checkout() {
+    # only clone if we haven't cloned already
+    if [ -d "${CHECKOUT_PATH}/.git" ]; then
+        echo "WARN: Repository seems to be cloned already, skipping..." >&2
+    else
+        git clone git@github.com:simplesurance/sisu.git --branch "$REPO_VERSION" "$CHECKOUT_PATH"
+    fi
+}
+
+function install() {
+    (cd "${CHECKOUT_PATH}/workspace" && make)
+}
+
+
+parse_args "$@"
+banner
+init
+checkout
+install
+
